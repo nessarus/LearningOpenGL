dd664239aa92de5d114904307690cb10b9004680
Joshua Ng <nessarus47@gmail.com>
2018-05-08 22:52:41 +0800

    episode 16

--- OpenGL/src/Shader.cpp
+++ OpenGL/src/Shader.cpp
@@ -12,8 +12,6 @@ Shader::Shader(const std::string & filepath)
 {
 	ShaderProgramSource source = ParseShader(filepath);
 	m_RendererID = CreateShader(source.VertexSource, source.FragmentSource);
-
-	
 }
 
 Shader::~Shader()
